e596e76ea98fdd45f2f90b6ad0d733795a055724
samuelmelo98 <samuelmelo98@hotmail.com>
2023-12-03 22:56:58 -0300

    Criando um componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { NovoComponenteComponent } from './novo-componente/novo-componente.component';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,MeuPrimeiro2Component],
+  imports: [CommonModule, RouterOutlet,MeuPrimeiro2Component, NovoComponenteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/novo-componente/novo-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-novo-componente',
+  standalone: true,
+  imports: [],
+  templateUrl: './novo-componente.component.html',
+  styleUrl: './novo-componente.component.css'
+})
+export class NovoComponenteComponent {
+
+}
